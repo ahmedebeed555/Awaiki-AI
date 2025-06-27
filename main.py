6f638889e430189bbcca32304d35f4af14a0b0e8
Ahmed Ebeed <69511205+ahmedebeed555@users.noreply.github.com>
2025-06-27 17:49:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import awaiki_ai
 
 # Replace 'VIDEO_LINK' with the holiest YouTube video link of your choice
-video_link = 'https://www.youtube.com/watch?v=77ZF50ve6rs'
+
+
+#video_link = 'https://www.youtube.com/watch?v=77ZF50ve6rs'
+
+video_link = 'https://www.youtube.com/watch?v=kCc8FmEb1nY'      # chatGPT from scratch
+
 api_key="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"
 
 awaiki_ai.analyze_and_convert(video_link,api_key)
